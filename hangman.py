66eeebc305885089eefd2c09f4c86cf2c88cc4bf
Syeda Tahira Fatima <55763747+TahiraFatima98@users.noreply.github.com>
2019-12-30 13:15:44 +0500

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,57 @@
+
+import time
+
+name = str(input("What is your name? "))
+
+print ("Hello, " + name, "Time to play hangman!")
+
+print ("")
+
+time.sleep(1)
+
+print ("Start guessing...")
+time.sleep(0.5)
+
+word = "secret"
+
+guesses = ''
+turns = 10
+
+while turns > 0:         
+
+
+    failed = 0             
+
+    for char in word:      
+
+        if char in guesses:    
+    
+            print (char),    
+
+        else:
+
+            print ("_"),     
+       
+            failed += 1    
+
+    if failed == 0:        
+        print ("you won") 
+
+        break              
+
+    print
+
+    guess = str(input("guess a character:"))
+
+    guesses += guess                    
+
+    if guess not in word:  
+
+        turns -= 1        
+
+        print ("Wrong")
+ 
+        print ("You have", + turns, 'more guesses' )
+ 
+        if turns == 0:           
+            print ("You Loose")
